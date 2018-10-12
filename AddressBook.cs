29fb30693c0d7fc7ae4f650d34849975496569f4
ThraxHamilton <mmcclenton16@gmail.com>
2018-10-12 11:42:42 -0500

    finished

--- AddressBook.cs
+++ AddressBook.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections.Generic;
 
 namespace AddressBook {
     public class AddressBook {
-        Dictionary<string, string> AddyBook = new Dictionary<string, string> ();
-        public void AddContact (Contact contact){
-            
-            AddyBook.Add(contact.FirstName, contact.Email);
-            
-        }
+        Dictionary<string, Contact> AddyBook = new Dictionary<string, Contact> ();
+       
+            public void AddContact (Contact contact) {
 
+                AddyBook.Add (contact.Email, contact);
+            }
+            public Contact GetByEmail (string email) {
+                return AddyBook[email];
+            }
+       
+        
     }
 }
